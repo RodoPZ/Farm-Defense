89bcae2e8ce89d574df0146071d39136d3054c86
RodoPZ <rpinedo2799@gmail.com>
2021-09-08 00:44:00 -0500

    Movimiento de torres

--- Godot/Scenes/Torres/Torres.gd
+++ Godot/Scenes/Torres/Torres.gd
@@ -1,19 +1,22 @@
 extends Node2D
-
-#cuadrantes
-#
-
+var mover = false	
+var pos_actual = self.position
 
 func _physics_process(delta):
-	turn()
-	
-func turn():
-	var enemy_pos = get_global_mouse_position()
-	var pos_actual = self.position
-	#1
-	if(enemy_pos[0] > pos_actual[0] and enemy_pos[1] < pos_actual[1]):
+	if (mover == true):	
+		var enemy_pos = get_global_mouse_position()
 		get_node("Torre").look_at(enemy_pos)
-	
-	
-	
 
+func _on_Rango_mouse_entered():
+	mover = true
+func _on_Rango_mouse_exited():
+	mover = false
+
+func _on_Abajo_mouse_entered():
+	print("Sprite_Abajo")
+func _on_Derecha_mouse_entered():
+	print("Sprite_Derecha")
+func _on_Izquierda_mouse_entered():
+	print("Sprite_Izquierda")
+func _on_Arriba_mouse_entered():
+	print("Sprite_Arriba")
