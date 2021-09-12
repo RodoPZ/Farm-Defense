b1c45029d569fe21d448c24112506c6e8c8d8d31
Croobat <tonyram9906@outlook.com>
2021-09-12 16:33:34 -0500

    Animaciones no se solapan, vacas pastan cada 5s, icono

--- Godot/Scenes/Torres/Vaca.gd
+++ Godot/Scenes/Torres/Vaca.gd
@@ -1,1 +1,12 @@
 extends "res://Scenes/Torres/Torres.gd"
+
+func _on_Timer_timeout():
+	if anim_free == true:
+		anim_free = false
+		animated_sprite.play("attack")
+	pass # Replace with function body.
+
+func _on_Torre_animation_finished():
+	if animated_sprite.animation == "attack":
+		anim_free = true
+		animated_sprite.play("idle")
