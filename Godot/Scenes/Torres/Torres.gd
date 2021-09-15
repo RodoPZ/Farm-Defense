bd3da59b27ae39bc3dfe062d6a4444e93d465cb0
RodoPZ <rpinedo2799@gmail.com>
2021-09-15 15:03:23 -0500

    Intentando arreglar animaciones part 2

--- Godot/Scenes/Torres/Torres.gd
+++ Godot/Scenes/Torres/Torres.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
 var spell = preload("res://Scenes/Projectiles/Semilla.tscn")
-var mover = false	
-var pos_actual = self.position
 var rot_actual
 var is_attacking
 var type
@@ -37,75 +35,33 @@ func _physics_process(_delta):
 		enemy = null
 
 func turn():
-	#get_node("Torre").look_at(enemy.position)
-	get_node("Torre").look_at(get_global_mouse_position())
+	get_node("Torre").look_at(enemy.position)
+	#get_node("Torre").look_at(get_global_mouse_position())
 	animate()
 
-	
-
-	"""
-	if 	not animated_sprite.flip_v and not animated_sprite.flip_h:
-		print("arriba")
-	elif animated_sprite.flip_v and not animated_sprite.flip_h:
-		print("izq")
-	elif not animated_sprite.flip_v and animated_sprite.flip_h:
-		print("izq")
-	"""
-	#if is_attacking == false and ready == false:
-		#is_attacking = true
-		#if (rot_actual > 45 and rot_actual < 135) or (rot_actual > -315 and rot_actual < -225):
-		#print(rot_actual)
-			#print("abajo")
-#		if is_attacking == false:
-#			is_attacking = true
-			#animated_sprite.play("attack_down")
-		#elif (rot_actual > 135 and rot_actual < 225) or (rot_actual > -225 and rot_actual < -135):
-		#print(rot_actual)
-			#print("izquierda")
-			#animated_sprite.flip_h = true
-#		if is_attacking == false:
-#			is_attacking = true
-			#animated_sprite.play("attack_right")
-		#elif (rot_actual > 225 and rot_actual < 315) or (rot_actual > -135 and rot_actual < -45):
-		#print(rot_actual)
-			#print("arriba")
-#		if is_attacking == false:
-#			is_attacking = true
-			#animated_sprite.play("attack_down")
-		#elif (rot_actual > 315 or rot_actual < 45) or (rot_actual > -45 or rot_actual < -315):
-		#print(rot_actual)
-			#print("derecha")
-#		if is_attacking == false:
-#			is_attacking = true
-			#animated_sprite.flip_h = false
-			#animated_sprite.play("attack_right")
 func animate():
-	print(	fmod(animated_sprite.get_rotation_degrees(),360))
+	rot_actual = fmod(animated_sprite.get_rotation_degrees(),360)
 	if ready == false:
 		is_attacking = true
-		if 45<animated_sprite.get_rotation_degrees() and animated_sprite.get_rotation_degrees()<135:
+		if (45<rot_actual and rot_actual<135) or (-315<rot_actual and rot_actual <-225):
 			animated_sprite.play("attack_down")
 			if animated_sprite.is_flipped_v() == true:
 				animated_sprite.flip_v = false
-		elif 135 < animated_sprite.get_rotation_degrees() and animated_sprite.get_rotation_degrees() < 225:
+		elif (135 < rot_actual and rot_actual < 225) or ( -225 <rot_actual and rot_actual <-135):
 			animated_sprite.play("attack_right")
 			if animated_sprite.is_flipped_v() == false:
 				animated_sprite.flip_v = true
-		if -135<animated_sprite.get_rotation_degrees() and animated_sprite.get_rotation_degrees()<-45:
+		elif (225<rot_actual and rot_actual<315) or ( -135 <rot_actual and rot_actual <-45):
 			animated_sprite.play("attack_up")
 			if animated_sprite.is_flipped_v() == true:
 				animated_sprite.flip_v = false
-		elif -45<animated_sprite.get_rotation_degrees() and animated_sprite.get_rotation_degrees()<45:
+		else: 
 			animated_sprite.play("attack_right")
 			if animated_sprite.is_flipped_v() == true:
 				animated_sprite.flip_v = false	
-		else:
-			if animated_sprite.is_flipped_v() == true:
-				animated_sprite.flip_v = false	 
 	else:
+		is_attacking = false
 		animated_sprite.play("idle")
-
-
 
 
 func select_enemy():
@@ -131,38 +87,15 @@ func fire():
 # si body se usa quitar el underscore
 func _on_Rango_body_entered(body):
 	enemy_array.append(body.get_parent())
-	mover = true
 	
 func _on_Rango_body_exited(body):
 	enemy_array.erase(body.get_parent())
-	mover = false
 	animated_sprite.set_rotation_degrees(0)
 	if is_attacking == false:
 		animated_sprite.play("idle")
 	if animated_sprite.is_flipped_v() == true:
 		animated_sprite.flip_v = false
 
-
-"""
-func _on_Derecha_body_entered(_body):
-	if anim_free == true:
-		anim_free = false
-		animated_sprite.flip_h = false
-		animated_sprite.play("attack_right")
-func _on_Abajo_body_entered(_body):
-	if anim_free == true:
-		anim_free = false
-		animated_sprite.play("attack_down")
-func _on_Izquierda_body_entered(_body):
-	if anim_free == true:
-		anim_free = false
-		animated_sprite.flip_h = true
-		animated_sprite.play("attack_right")
-func _on_Arriba_body_entered(_body):
-	if anim_free == true:
-		anim_free = false
-		animated_sprite.play("attack_up")
-"""
 
 func _on_Torre_animation_finished():
 		is_attacking = false
